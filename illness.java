5d051e899728d873eb8d96b4a043e4c9f58c216b
Oksana-deva <144332419+Oksana-deva@users.noreply.github.com>
2024-02-11 21:27:32 +0300

    Add files via upload

---
+++ illness.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Illness {
+
+    String title;
+
+    public Illness(String illness) {
+        title = illness;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
